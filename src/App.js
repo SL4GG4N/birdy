80b286c6de83243d203a4fd01afd4a342757b724
SL4GG4N <eddie.lowenborg@live.se>
2017-09-20 16:25:47 +0200

    Tutorial done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {inject, observer} from 'mobx-react';
 
+@inject('BirdStore')
+@observer
 class App extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const bird = this.bird.value;
+    this.props.BirdStore.addBird(bird);
+    this.bird.value = "";
+  }
+
   render() {
+    const {BirdStore} = this.props;
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>You have {BirdStore.birdCount} birds.</h2>
+
+      <form onSubmit={e => this.handleSubmit(e)}>
+        <input type="text" placeholder="Enter Bird" ref={input => this.bird = input}></input>
+        <button>Add bird</button>
+      </form>
+
+      <ul>
+        {BirdStore.birds.map(bird =>(
+          <li key={bird}>
+            {bird}
+          </li>
+        ))}
+      </ul>
       </div>
     );
   }
--- src/stores/BirdStore.js
+++ src/stores/BirdStore.js
@@ -3,12 +3,12 @@ import {observable, action, computed} from 'mobx';
 class BirdStore {
   @observable birds = [];  // data I want to keep track on
 
-  @action addBird = (bird) => {
+  @action addBird = (bird) => { //actions are for changing observables
     this.birds.push(bird);
   }
 
-  @computed get birdCount() => {
-    return this.birds.size;
+  @computed get birdCount(){ // perform calculations on observables
+    return this.birds.length;
   }
 }
 
